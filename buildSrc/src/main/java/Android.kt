dc42670558a17062754310854418d3d89cfdc783
Rishi <thezerostartup@gmail.com>
2024-01-16 16:50:09 +0530

    Minor Updates

--- buildSrc/src/main/java/Android.kt
+++ buildSrc/src/main/java/Android.kt
@@ -3,6 +3,6 @@ object Android {
     const val compileSdk = 34
    const val minSdk = 21
     const val targetSdk = 34
-    const val versionCode = 146
-    const val versionName = "64.6.9"
+    const val versionCode = 147
+    const val versionName = "64.7.0"
 }
